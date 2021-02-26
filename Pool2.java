c4f2c0be38616cf1212611af830a6efaa097d4a4
David Mack <mack.david.88@gmail.com>
2021-02-26 22:04:15 +0100

    :sparkles: Pool 2 started. bug: excessive kids in the pool

--- Pool2.java
+++ Pool2.java
@@ -1,9 +1,96 @@
 // CSD feb 2015 Juansa Sendra
 
 public class Pool2 extends Pool{ //max kids/instructor
-    public void init(int ki, int cap)           {}
-    public void kidSwims()      {log.swimming();}
-    public void kidRests()      {log.resting(); }
-    public void instructorSwims()   {log.swimming();}
-    public void instructorRests()   {log.resting(); }
+  protected int kidsInPool;
+  protected int instructorsInPool;
+  protected int max_cap;
+  protected int cur_cap;
+  protected int max_ki;
+  protected int cur_ki;
+
+  @Override
+  public void init(int ki, int cap) {
+    kidsInPool = 0;
+    instructorsInPool = 0;
+    cur_ki = 0;
+    max_ki = ki;
+    cur_cap = 0;
+    max_cap = cap;
+  }
+
+  @Override
+  public synchronized void kidSwims() throws InterruptedException {
+    System.out.println("kid want to swim");
+    System.out.println("kids presents: " + kidsInPool);
+    System.out.println("instructors presents: " + instructorsInPool);
+    System.out.println("bef ki: " + ((instructorsInPool == 0) ? 0 : kidsInPool/instructorsInPool));
+    System.out.println("aft ki: " + ((instructorsInPool == 0) ? 0 : (kidsInPool+1)/instructorsInPool));
+
+    while ( instructorsInPool <= 0 || (instructorsInPool == 0) ? true : max_ki < (kidsInPool+1)/instructorsInPool){
+      log.waitingToSwim();
+      wait();
+    }
+
+
+    // update state
+    kidsInPool++;
+    System.out.println("ki after kid start swim: " + ((instructorsInPool == 0) ? 0 : kidsInPool/instructorsInPool));
+    // awake threads if needed
+
+    log.swimming();
+  }
+
+  @Override
+  public synchronized void kidRests() {
+    System.out.println("kid want to rest");
+    System.out.println("ki before kid go to rest: " + ((instructorsInPool == 0) ? 0 : kidsInPool/instructorsInPool));
+    kidsInPool--;
+
+    // if (kidsInPool <= 0)
+    notify();   // let the last instructor rest if he was waiting
+
+    System.out.println("ki after kid go to rest: " + ((instructorsInPool == 0) ? 0 : kidsInPool/instructorsInPool));
+    log.resting();
+  }
+
+  @Override
+  public synchronized void instructorSwims() {
+    System.out.println("instructor want to swim");
+    System.out.println("ki before instr start swim: " + ((instructorsInPool == 0) ? 0 : kidsInPool/instructorsInPool));
+    instructorsInPool++;
+
+    // if (kidsInPool <= 0 && max_ki >= kidsInPool/instructorsInPool)0
+    notifyAll();        // let all the kids waiting to start swimming
+
+    System.out.println("ki after notifyAll the kids: " + ((instructorsInPool == 0) ? 0 : kidsInPool/instructorsInPool));
+
+    log.swimming();
+  }
+
+  @Override
+  public synchronized void instructorRests() throws InterruptedException {
+    System.out.println("instructor want to rest");
+    System.out.println("instructors presents: " + instructorsInPool);
+    System.out.println("kids presents: " + kidsInPool);
+    System.out.println("bef ki: " + ((instructorsInPool == 0) ? 0 : kidsInPool/instructorsInPool));
+    System.out.println("aft ki: " + ((instructorsInPool == 0) ? 0 : (instructorsInPool-1 == 0) ? 0 : kidsInPool/(instructorsInPool-1)));
+
+    while ( (kidsInPool > 0 && instructorsInPool <= 1) || (instructorsInPool == 0) ? false : (instructorsInPool-1 == 0) ? false : max_ki <= kidsInPool/(instructorsInPool-1)){
+      log.waitingToRest();
+      wait();
+    }
+
+    // update state
+    if (instructorsInPool == 0) {
+      instructorsInPool--;
+      System.out.println("ki after instructor go to rest: " + ((instructorsInPool == 0) ? 0 : kidsInPool/instructorsInPool));
+
+      // awake threads if needed
+
+      log.resting();
+    }
+    else
+      System.out.println("\n\n WARNING instructor already resting \n\n");
+
+  }
 }
--- Pool3.java
+++ Pool3.java
@@ -1,9 +1,74 @@
 // CSD feb 2015 Juansa Sendra
 
-public class Pool3 extends Pool{ //max capacity
-    public void init(int ki, int cap)           {}
-    public void kidSwims()      {log.swimming();}
-    public void kidRests()      {log.resting(); }
-    public void instructorSwims()   {log.swimming();}
-    public void instructorRests()   {log.resting(); }
+public class Pool3 extends Pool {   //max capacity
+    protected int kidsInPool;
+    protected int instructorsInPool;
+    protected int max_cap;
+    protected int max_ki;
+
+    @Override
+    public void init(int ki, int cap) {
+      kidsInPool = 0;
+      instructorsInPool = 0;
+      max_ki = ki;
+      max_cap = cap;
+    }
+
+    @Override
+    public synchronized void kidSwims() throws InterruptedException {
+      System.out.println("kid want to swim");
+      System.out.println("kids presents: " + kidsInPool);
+      System.out.println("instructors presents: " + instructorsInPool);
+
+      while ( instructorsInPool <= 0 && max_ki <= kidsInPool/instructorsInPool){
+        log.waitingToSwim();
+        wait();
+      }
+
+      // update state
+      kidsInPool++;
+      // awake threads if needed
+
+      log.swimming();
+    }
+
+    @Override
+    public synchronized void kidRests() {
+      System.out.println("kid want to rest");
+      kidsInPool--;
+
+      if (kidsInPool <= 0)
+        notify();   // let the last instructor rest if he was waiting
+
+      log.resting();
+    }
+
+    @Override
+    public synchronized void instructorSwims() {
+      System.out.println("instructor want to swim");
+      instructorsInPool++;
+
+      if (kidsInPool <= 0)
+        notifyAll();        // let all the kids waiting to start swimming
+
+      log.swimming();
+    }
+
+    @Override
+    public synchronized void instructorRests() throws InterruptedException {
+      System.out.println("instructor want to rest");
+      System.out.println("instructors presents: " + instructorsInPool);
+      System.out.println("kids presents: " + kidsInPool);
+      while ( kidsInPool > 0 && instructorsInPool <= 1 && max_ki <= kidsInPool/instructorsInPool){
+        log.waitingToRest();
+        wait();
+      }
+
+      // update state
+      instructorsInPool--;
+
+      // awake threads if needed
+
+      log.resting();
+    }
 }
